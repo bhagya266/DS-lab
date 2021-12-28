fde5ed7578088f0b332b4892a02e679570853203
bhagya266 <85730381+bhagya266@users.noreply.github.com>
2021-12-28 15:44:37 +0530

    Update linear_serach.c

--- linear_serach.c
+++ linear_serach.c
@@ -1,42 +1,108 @@
-#include <stdio.h>
-int RecursiveLS(int arr[], int value, int index, int n)
-{
-    int pos = 0;
-    if(index >= n)
-    {
-        return 0;
-    }
-    else if (arr[index] == value)
-    {
-        pos = index + 1;
-        return pos;
-    }
-    else
-    {
-        return RecursiveLS(arr, value, index+1, n);
-    }
-    return pos;
-}
-int main()
-{
-    int n, value, pos, m = 0, arr[100];
-    printf("Enter the total elements in the array  ");
-    scanf("%d", &n);
-    printf("Enter the array elements\n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    printf("Enter the element to search  ");
-    scanf("%d", &value);
-    pos =  RecursiveLS(arr, value, 0, n);
-    if (pos != 0)
-    {
-        printf("Element found at pos %d ", pos);
-    }
-    else
-    {
-        printf("Element not found");
-    }
-    return 0;
-}
+i)Linear Search
+Using Recursion
+  
+ 	include<stdio.h>
+int linearSearch1(int[] ,int , int);
+	int main(){
+	        int a[30],n,i,x,p;
+	        printf("Enter size of list:");
+	        scanf("%d" , &n);
+	        printf("Elements of array:");
+	        for(i=0;i<n;i++){
+	                scanf("%d" ,&a[i]);
+	        }
+	        printf("Element to be searched:");
+	        scanf("%d",&x);
+	        p = linearSearch1(a,n,x);
+	        if(p == -1){
+	             printf("Element not found");
+	        }
+	        else{
+	             printf("Element found at position %d :",p);
+	        }
+	}
+	int i=0;
+	int linearSearch1(int a[],int n,int x){
+	        if(a[i]==x){
+	                return i;
+	        }
+	        if(i<n){
+	                i++;
+	                linearSearch1(a,n,x);
+	        }
+	        if(i>=n){
+	                return -1;
+	        }
+	}
+	
+
+	Input:
+	Enter size of list:3
+	Elements of array:1
+	2
+	3
+	Element to be searched:3
+	Output:
+	Element found at position 2
+	
+
+	Input:
+	Enter size of list:3
+	Elements of array:1
+	2
+	3
+	Element to be searched:4
+	Output:
+	Element not found
+
+Using Non Recursion 
+ 
+	include<stdio.h>
+int linearSearch(int[] ,int , int);
+	int main(){
+	        int a[30],n,i,x,p;
+	        printf("Enter size of list:");
+	        scanf("%d", &n);
+	        printf("Elements of array:");
+	        for(i=0;i<n;i++){
+	                scanf("%d" ,&a[i]);
+	        }
+	        printf("Element to be searched:");
+	        scanf("%d",&x);
+	        p = linearSearch(a,n,x);
+	        if(p == -1){
+	             printf("Element not found");
+	        }
+	        else{
+	             printf("Element found at position %d :",p);
+	       }
+	}
+	int linearSearch(int a[],int n,int x){
+	        int i;
+	        for(i=0;i<n;i++){
+	                if(x == a[i]){
+	                        return i;
+	                }
+	        }
+	        return -1;
+	}
+	
+
+	Input:
+	Enter size of list:3
+	Elements of array:2
+	3
+	1
+	Element to be searched:3
+	Output:
+	Element found at position 1
+	
+
+	Input:
+	Enter size of list:2
+	Elements of array:1
+	2
+	Element to be searched:3
+	Output:
+	Element not found
+
